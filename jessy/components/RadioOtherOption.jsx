847b30c7a012b5ea0399a633be2895163c0ed6c5
illichso <illichso@gmail.com>
2015-12-13 22:24:19 +0100

    add new RadioOtherOption component

---
+++ jessy/components/RadioOtherOption.jsx
@@ -0,0 +1,18 @@
+var RadioOtherOption = React.createClass({
+    render: function () {
+        return (
+
+            <p className="form-group radio">
+                <label>
+                    <input type="radio" name="referrer" value="other"/>
+                    Other
+                </label>
+                <label className="form-inline">
+                    Please specify:
+                    <input type="text" name="referrer_other"/>
+                </label>
+            </p>
+        );
+    }
+
+});
