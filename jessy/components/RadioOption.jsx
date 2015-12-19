40507006459cfa4ea7a2bd03e1bd80251b97feb6
illichso <illichso@gmail.com>
2015-12-19 22:19:40 +0100

    add props

--- jessy/components/RadioOption.jsx
+++ jessy/components/RadioOption.jsx
@@ -1,4 +1,8 @@
 var RadioOption = React.createClass({
+    propTypes: {
+        value: React.PropTypes.string.isRequired,
+        children: React.PropTypes.node
+    },
     render: function () {
         return (
             <p className="radio">
